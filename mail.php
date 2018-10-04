3c68169546fb692b2148ed8aa96e426a09e9d1c4
Arttic Fox <bora.alap@artticfox.com>
2018-10-04 01:02:44 -0400

    adding from

--- mail.php
+++ mail.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('ersoyalap@gmail.com'); // ← Your email here
+$pp->sendEmailTo('alapbora@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
